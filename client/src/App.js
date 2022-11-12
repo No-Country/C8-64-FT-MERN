6b144f2d2bf8835459c230a5d0893e547f467f27
cerveux <diegomartinguzman@hotmail.com.ar>
2022-11-12 11:50:28 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './nc-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        C8-64-FT-MERN
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
